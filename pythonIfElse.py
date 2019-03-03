85119e9c126fa97f3077d3d33bc6a3f54461a781
Aylin Akseki <aylinakseki@Aylins-MacBook-Air.local>
2019-03-04 00:09:04 +0300

    Added Python If Else

---
+++ pythonIfElse.py
@@ -0,0 +1,10 @@
+N = int(input("Enter a number:"))
+if(N % 2)==1:
+    print("Weird")
+elif(((N % 2) == 0) and ( N >= 2 and N <= 5)):
+    print("Not Weird")
+elif(((N % 2) == 0) and ( N >= 6 and N <= 20)):
+    print("Weird")
+elif(((N % 2) == 0) and ( N > 20)):
+     print("Not Weird")
+    
